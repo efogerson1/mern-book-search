8e46b7969199a9d747079daaff096dce41649049
efogerson1 <efogerson2@gmail.com>
2023-10-02 15:27:39 -0700

    added dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -12,16 +12,16 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-// Construct GraphQL API endpoint
+// Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if exists
+  // get the authentication token from local storage if it exists
   const token = localStorage.getItem('id_token');
-  // return the headers to context
+  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
@@ -31,7 +31,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-  // setup `authLink` middleware prior to making the request to our GraphQL API
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
@@ -63,4 +63,3 @@ function App() {
 }
 
 export default App;
-
